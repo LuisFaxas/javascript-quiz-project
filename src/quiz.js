4a4f343eb093eb6565fb8bf1cc9df74fcbd2d1e0
LuisFaxas <faxaslive@gmail.com>
2024-02-12 18:28:31 -0500

    done

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,40 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor(questions, timeLimit,timeRemaining){
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+    }
     
-    // 3. moveToNextQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
+    
+    moveToNextQuestion(){
+        this.currentQuestionIndex ++
+    }
+    
+    shuffleQuestions(){
+        for(let i = this.questions.length; i > 0; i--){
+            const j = Math.floor(Math.random() * (i+1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], [this.questions[i]]]
+        }
+    }
 
-    // 4. shuffleQuestions()
+    checkAnswer(answer){
+        const currentQuestion = this.questions[this.currentQuestionIndex];
+        if(currentQuestion.answer === answer){
+            this.correctAnswers++;
+        }
 
-    // 5. checkAnswer(answer)
+    }
 
-    // 6. hasEnded()
+    hasEnded() {
+
+        return this.currentQuestionIndex === this.questions.length;
+
+        }
 }
